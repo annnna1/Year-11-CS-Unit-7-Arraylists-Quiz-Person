53409d6ea12aea62a969fff30620fdeef98209ea
annnna1 <123133994+annnna1@users.noreply.github.com>
2023-03-06 21:18:12 -0800

    Update PersonFactory.java

--- src/PersonFactory.java
+++ src/PersonFactory.java
@@ -1,22 +1,51 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class PersonFactory {
 
     private ArrayList<Person> allPersons;
 
-    public PersonFactory() {
+    public PersonFactory()
+    {
         allPersons = new ArrayList<>();
     }
 
-    public void addPerson(Person person) {
-
+    public void addPerson(Person person)
+    {
+        allPersons.add(person);
+        for(int i = allPersons.size()-2 ; i >=0 ; i --)
+        {
+            Person person2 = allPersons.get(i);
+            if(person.getLastName().compareTo(person2.getLastName())==-1)
+            {
+                allPersons.set(i, person);
+                allPersons.add(i + 1, person2);
+            }
+        }
     }
 
-    public ArrayList<Person> under18() {
-
+    public ArrayList<Person> under18()
+    {
+        ArrayList<Person> under18 = new ArrayList<>();
+        for(int i = 0 ; i < allPersons.size() ; i++)
+        {
+            if(allPersons.get(i).getAge()<18)
+            under18.add(allPersons.get(i));
+        }
+        return under18;
     }
 
-    public boolean checkLastName(String lastName) {
+    public boolean checkLastName(String lastName)
+    {
+        boolean bool = false;
+        for(int i = 0 ; i < allPersons.size() ; i++)
+        {
+            if(lastName.equals(allPersons.get(i).getLastName()))
+            {
+                bool = true;
+            }
 
+        }
+        return bool;
     }
 }
